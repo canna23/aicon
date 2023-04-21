f45bfb34a74ae2ccd119c89f93d0d745a7631fa6
canna23 <130270556+canna23@users.noreply.github.com>
2023-04-21 21:06:09 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+import { ThemeProvider } from "@aws-amplify/ui-react";
+import { Amplify } from 'aws-amplify';
+
+import awsconfig from './aws-exports';
+
+import "@aws-amplify/ui-react/styles.css";
+import { studioTheme } from "./ui-components";
+
+Amplify.configure(awsconfig);
+
 console.log("index.js: loaded");
